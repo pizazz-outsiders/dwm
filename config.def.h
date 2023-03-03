8b1c92a61fcad8ff11c9764a5d94c946165d6997
aaron-win <aaron-win@email.com>
2023-03-03 20:26:40 +0800

    add scratchpad

--- config.def.h
+++ config.def.h
@@ -66,12 +66,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	/* modifier                     key            function        argument */
+	{ MODKEY,                       XK_p,          spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,     spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return,     zoom,           {0} },
+	{ MODKEY,                       XK_apostrophe, togglescratch,  {.v = scratchpadcmd } },
 
 	{ MODKEY|ShiftMask,             XK_q,      killclient,           {0} },
 	{ MODKEY|ShiftMask|ControlMask, XK_c,      quit,     {0} },
