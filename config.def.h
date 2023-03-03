b5d45dd0272b8c05618123375110eb3e18bb0ffe
aaron-win <aaron-win@email.com>
2023-03-03 20:28:10 +0800

    add alpha

--- config.def.h
+++ config.def.h
@@ -13,6 +13,8 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "Hack Nerd Font Mono:size=20" };
 static const char dmenufont[]       = "monospace:size=10";
+static unsigned int baralpha        = 0xd0;
+static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
